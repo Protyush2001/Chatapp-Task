8e0f276e4a05f2d15b2807c08ffce05cd8f6707f
Protyush2001 <deyprotyush@gmail.com>
2026-01-03 13:46:13 +0530

    Added search filter in createRoom modal

--- Frontend/src/components/CreateRoomModal.jsx
+++ Frontend/src/components/CreateRoomModal.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 export default function CreateRoomModal({
   users,
   userId,
@@ -8,6 +10,10 @@ export default function CreateRoomModal({
   createRoom,
   onClose
 }) {
+
+  const [search,setSearch] = useState("");
+  const filteredUsers = users.filter(u => u._id !== userId).filter(u => u.username.toLowerCase().includes(search.toLowerCase()))
+  
   return (
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
       <div className="bg-white p-5 rounded w-80">
@@ -20,27 +26,39 @@ export default function CreateRoomModal({
           className="w-full border px-3 py-2 rounded mb-3"
         />
 
+        <input type="text" value={search} onChange={e => setSearch(e.target.value)}
+        placeholder="Search users..."
+        className="w-full border px-3 py-2rounded mb-3 text-sm"
+        />
+
         <div className="max-h-40 overflow-y-auto border rounded p-2 mb-3">
-          {users
-            .filter(u => u._id !== userId)
-            .map(u => (
-              <label key={u._id} className="flex items-center gap-2 mb-1">
-                <input
-                  type="checkbox"
-                  checked={selectedUsers.includes(u._id)}
-                  onChange={e => {
-                    if (e.target.checked) {
-                      setSelectedUsers(prev => [...prev, u._id]);
-                    } else {
-                      setSelectedUsers(prev =>
-                        prev.filter(id => id !== u._id)
-                      );
-                    }
-                  }}
-                />
-                <span className="text-sm">{u.username}</span>
-              </label>
-            ))}
+          {filteredUsers.length === 0 && (
+            <p className="text-xs text-gray-400 text-center">
+              No users found
+            </p>
+          )}
+
+          {filteredUsers.map(u => (
+            <label
+              key={u._id}
+              className="flex items-center gap-2 mb-1 cursor-pointer"
+            >
+              <input
+                type="checkbox"
+                checked={selectedUsers.includes(u._id)}
+                onChange={e => {
+                  if (e.target.checked) {
+                    setSelectedUsers(prev => [...prev, u._id]);
+                  } else {
+                    setSelectedUsers(prev =>
+                      prev.filter(id => id !== u._id)
+                    );
+                  }
+                }}
+              />
+              <span className="text-sm">{u.username}</span>
+            </label>
+          ))}
         </div>
 
         <div className="flex gap-2">
