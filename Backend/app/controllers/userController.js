17da241495dc45a741ce4f5827c6b1d93f0224ee
Protyush2001 <deyprotyush@gmail.com>
2026-01-23 20:22:23 +0530

    pagination update

--- Backend/app/controllers/userController.js
+++ Backend/app/controllers/userController.js
@@ -2,31 +2,59 @@ const User = require("../models/user");
 
 const userCtlr = {};
 
+// userCtlr.getAllUsers = async (req, res) => {
+//   // try {
+//   //   const users = await User.find(
+//   //     {}, 
+//   //     "_id username email"
+//   //   );
+
+//   //   return res.status(200).json(users);
+//   // } catch (err) {
+//   //   console.error(err);
+//   //   return res.status(500).json({ message: "Server error" });
+//   // }
+
+//       try{
+//         const page = parseInt(req.query.page) || 1;
+//         const limit = parseInt(req.query.limit) || 5;
+//         const skip = (page - 1) * limit;
+//         const users = await User.find()
+//         .select("-password")
+//         .skip(skip)
+//         .limit(limit);
+//         return res.status(200).json(users);
+//     }catch(err){
+//         return res.status(400).json({message:"Something went wrong"});
+//     }
+// };
+
 userCtlr.getAllUsers = async (req, res) => {
-  // try {
-  //   const users = await User.find(
-  //     {}, 
-  //     "_id username email"
-  //   );
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
 
-  //   return res.status(200).json(users);
-  // } catch (err) {
-  //   console.error(err);
-  //   return res.status(500).json({ message: "Server error" });
-  // }
+    const users = await User.find()
+      .select("-password")
+      .skip(skip)
+      .limit(limit);
 
-      try{
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 5;
-        const skip = (page - 1) * limit;
-        const users = await User.find()
-        .select("-password")
-        .skip(skip)
-        .limit(limit);
-        return res.status(200).json(users);
-    }catch(err){
-        return res.status(400).json({message:"Something went wrong"});
-    }
+    const total = await User.countDocuments();
+
+    return res.status(200).json({
+      users,
+      pagination: {
+        page,
+        limit,
+        total,
+        hasMore: skip + users.length < total
+      }
+    });
+  } catch (err) {
+    return res.status(400).json({ message: "Something went wrong" });
+  }
 };
+
 
 module.exports = userCtlr;
